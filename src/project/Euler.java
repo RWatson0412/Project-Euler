e2cf8cfc605308f3902cd2ed97501526381bacdb
Ryan-DT <Ryan-DT@192.168.0.23>
2018-05-01 22:13:06 -0500

    General format created

--- src/project/Euler.java
+++ src/project/Euler.java
@@ -1,10 +1,30 @@
+//All problems taken from projecteuler.net
+
 package project;
+
+import java.util.Scanner;
 
 public class Euler {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Which question would you like to have answered?");
+		int i = scan.nextInt();
+		System.out.println("The answer to question #" + i + " is " + getAnswer(i));
+		if(getAnswer(i) == -1) {
+			System.out.println("It looks like that question either doesn't exist or hasn't been answered yet.");
+		}
+		
+	}
 
+	public static int getAnswer(int i) {
+		if(i == 1) return firstAnswer();
+		
+		return -1;
+	}
+	
+	public static int firstAnswer() {
+		return 0;
 	}
 
 }
